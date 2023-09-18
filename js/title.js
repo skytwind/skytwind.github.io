8b5953f16aa6e66cfc17b3abd65f07002e631864
skytwind <2067878944@qq.com>
2023-09-18 22:47:23 +0800

    Site updated: 2023-09-18 22:47:23

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    //离开当前页面时标签显示内容
-    document.title = 'ヽ(*。>Д<)o゜再来看看嘛~';
-    clearTimeout(titleTime);
-  } else {
-    //返回当前页面时标签显示内容
-    document.title = '(oﾟvﾟ)ノ抓到你啦～';
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="ヽ(*。>Д<)o゜再来看看嘛~",clearTimeout(titleTime)):(document.title="(oﾟvﾟ)ノ抓到你啦～",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
